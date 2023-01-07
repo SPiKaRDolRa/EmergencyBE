30ae8eb1c1bf40fd687fcd53702d718e3b308c8a
SPiKaRDolRa <become_aum@hotmail.com>
2023-01-07 17:37:46 +0700

    fix: body pases traffic problem

--- routes/create_traffic_problem_report.js
+++ routes/create_traffic_problem_report.js
@@ -8,7 +8,7 @@ router.use(bodyParser.json());
 router.post("/", (req, res) => {
   const bodyReport = req.body;
   let insertTrafficProblemReport = `INSERT INTO traffic_problem_report (problem_category, other_info, lnt, lng, img, create_at)
-                               VALUES('${bodyReport.problemCategory}', '${bodyReport.otherInfo}', ${bodyReport.lnt}, ${bodyReport.lng}, '${bodyReport.img}', '${bodyReport.img}', '${bodyReport.createAt}')`;
+                               VALUES('${bodyReport.problemCategory}', '${bodyReport.otherInfo}', ${bodyReport.lnt}, ${bodyReport.lng}, '${bodyReport.img}', '${bodyReport.createAt}')`;
 
   db.query(insertTrafficProblemReport, (err, result) => {
     if (err) {
