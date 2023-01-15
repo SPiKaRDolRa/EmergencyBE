65274a6d1fcb23130b3330b98b243407b883bad2
SPiKaRDolRa <become_aum@hotmail.com>
2023-01-15 14:15:24 +0700

    feat: final additional request

--- routes/create_emergency_report.js
+++ routes/create_emergency_report.js
@@ -7,8 +7,8 @@ router.use(bodyParser.json());
 
 router.post("/", (req, res) => {
   const bodyReport = req.body;
-  let insertEmergencyReport = `INSERT INTO emergency_report (victim_vehicle, has_parties, party_vehicle, accident_info, lnt, lng, img, create_at)
-                               VALUES('${bodyReport.victimVechicle}', ${bodyReport.hasParties}, '${bodyReport.partyVehicle}', '${bodyReport.accidentInfo}', ${bodyReport.lnt}, ${bodyReport.lng}, '${bodyReport.img}', '${bodyReport.createAt}')`;
+  let insertEmergencyReport = `INSERT INTO emergency_report (victim_vehicle, has_parties, party_vehicle, accident_info, other_info, assumption, img, lnt, lng, create_at)
+                               VALUES('${bodyReport.victimVechicle}', ${bodyReport.hasParties}, '${bodyReport.partyVehicle}', '${bodyReport.accidentInfo}', '${bodyReport.otherInformation}', '${bodyReport.assumption}', '${bodyReport.img}', ${bodyReport.lnt}, ${bodyReport.lng}, '${bodyReport.createAt}')`;
 
   db.query(insertEmergencyReport, (err, result) => {
     if (err) {
